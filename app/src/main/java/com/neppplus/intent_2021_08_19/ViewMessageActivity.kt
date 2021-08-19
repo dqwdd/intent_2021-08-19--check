05cb4d99dec0677f3fccb1b99431e9e575155b9d
유병재 <dqwdd@naver.com>
2021-08-19 15:53:26 +0900

    전달 받은 String 데이터를 UI에 반영 예시

--- app/src/main/java/com/neppplus/intent_2021_08_19/ViewMessageActivity.kt
+++ app/src/main/java/com/neppplus/intent_2021_08_19/ViewMessageActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.intent_2021_08_19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,9 +10,13 @@ class ViewMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_message)
 
 
-        var myName = "YBJ"
+//        화면에 들어오자마자 첨부된 데이터 있는지 확인부터 하고 저장하자
+        val receivedMessage = intent.getStringExtra("이름표임")
+//        intent로 왔으니까 intent 쓰고 스트링값이엇는진 모르지만 겟 스트링으로 받고
+//        ""사이는 꼬리표 이름 쓰는거임
 
-
+//        이제 UI에 반영
+        messageTxt.text = receivedMessage
 
 
     }
