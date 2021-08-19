5382f906d88ab06add915ca07ea1d651f47c899d
유병재 <dqwdd@naver.com>
2021-08-19 15:42:49 +0900

    Intent(2) 연습 UI 작업

---
+++ app/src/main/java/com/neppplus/intent_2021_08_19/ViewMessageActivity.kt
@@ -0,0 +1,18 @@
+package com.neppplus.intent_2021_08_19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_message)
+
+
+        var myName = "YBJ"
+
+
+
+
+    }
+}
